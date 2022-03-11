5dfc556ec7ef1d8eeee4c5b901e1186fa9179c6a
Socrates-Gebremedhin <socregebre@gmail.com>
2022-03-11 04:41:49 -0800

    :) :)

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf(" %i is positive\n", n);
+printf(" %lu is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%i is zero\n", n);
+printf("%lu is zero\n", n);
 }
 else
 {
-printf("%i is negative\n", n);
+printf("%lu is negative\n", n);
 }
 return (0);
 }
