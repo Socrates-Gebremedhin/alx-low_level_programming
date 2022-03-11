5dc88ab4494a26ddcc439f2657bd283dbf611d4d
Socrates-Gebremedhin <socregebre@gmail.com>
2022-03-11 00:41:49 -0800

    push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char c;
 long int  l;
 long long int ll;
 float f;
-printf("Size of a char: ", (unisigned long)sizeof(c)," byte(s)");
-printf("Size of a int: ", (unisigned long)sizeof(c)," byte(s)");
-printf("Size of a long int: ", (unisigned long)sizeof(c)," byte(s)");
-printf("Size of a long long int: ", (unisigned long)sizeof(c)," byte(s)");
-printf("Size of a float: ", (unisigned long)sizeof(c)," byte(s)");
+printf("Size of a char: ", (unsigned long)sizeof(c)," byte(s)");
+printf("Size of a int: ", (unsigned long)sizeof(c)," byte(s)");
+printf("Size of a long int: ", (unsigned long)sizeof(c)," byte(s)");
+printf("Size of a long long int: ", (unsigned long)sizeof(c)," byte(s)");
+printf("Size of a float: ", (unsigned long)sizeof(c)," byte(s)");
 }
 
